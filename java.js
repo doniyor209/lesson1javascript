a2a5f497c2dd394ef424f1931b326e2916a44a25
doniyor209 <doniyortoshev123@gmail.com>
2025-02-20 14:26:06 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+
+var a = 15;
+let ism = "Doniyor";
+const yil = 2025;
+
+
+
+
+let ism2 = prompt("Ismingizni kiriting;")
+let familiya = prompt("Familiyangizni kiriting;")
+
+console.log("Sizning ismingiz:" + ism2);
+console.log("Sizning familyangiz:" + familiya);
+let yosh = prompt("Sizning yoshingiz;")
+let tuman = prompt("Sizning yashayotgan tumaningiz;")
+
+console.log("Sizning yoshingiz:" + yosh);
+console.log("Sizning yashayotgan tumaningiz:" + tuman);
+
+
+
+alert("Malumotlar Uchun rahmat")
